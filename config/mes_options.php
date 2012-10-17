76d2fe2709b8ced93adc3914bab54b47fdc4e562
abelass <websolutions@mychacra.net>
2012-10-17 18:05:44 +0200

    nettoyage

--- config/mes_options.php
+++ config/mes_options.php
@@ -1,4 +1,10 @@
 <?php
-  define ('_ID_WEBMESTRES',
+// Partie reservee au Couteau Suisse. Ne pas modifier, merci
+// Please don't modify; this code is auto-generated
+@include_once "/home/abelass/www/spip_2.1/sites/muziekpublique/tmp/couteau-suisse/mes_spip_options.php";
+if($GLOBALS['cs_spip_options']) define('_CS_SPIP_OPTIONS_OK',1);
+// Fin du code. Ne pas modifier ces lignes, merci
+
+define ('_ID_WEBMESTRES',
   '1:35');
 ?>
