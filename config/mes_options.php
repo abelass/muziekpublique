0660bf3b44dae87fcf2cd4c3b18790acf3928de8
abelass <websolutions@mychacra.net>
2012-10-25 16:55:53 +0200

    filtrer essai

--- config/mes_options.php
+++ config/mes_options.php
@@ -4,7 +4,7 @@
 @include_once "/home/abelass/www/spip_2.1/sites/muziekpublique/tmp/couteau-suisse/mes_spip_options.php";
 if($GLOBALS['cs_spip_options']) define('_CS_SPIP_OPTIONS_OK',1);
 // Fin du code. Ne pas modifier ces lignes, merci
-
+$table_des_traitements['URL_PHOTO'][]= 'extraire_multi(%s)';
 define ('_ID_WEBMESTRES',
   '1:35');
 ?>
