20febe3ff54256fe096dd28f761e87b575dcf560
abelass <websolutions@mychacra.net>
2013-04-24 15:23:33 +0200

    rapatrier les fichiers de traduc

---
+++ squelettes/lang/bp/local_nl.php
@@ -0,0 +1,319 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nommé  genere le NOW()
+// langue / language = nl
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+// A
+'a' => 'tot',
+'a_partir_de' => 'van',
+'acces_liens_refuse' => 'Volg de aanwijzingen hieronder te kopen deze album en toegang te krijgen tot de download link',
+'accueil' => 'Home',
+'achat_cd' => 'Koop dit album!',
+'action' => 'Actions',
+'actions_autres' => 'Autres actions',
+'actions_requetes' => 'Actions requetes',
+'activite_reservation_message_confirmation_inscription' => 'Hallo @nom@,
+
+Muziekpublique dankt u voor uw reservatie. 
+
+Activiteit : @activite@
+Datum: @date@
+Gereserveerd op naam van: @nom@
+
+@inscriptions@
+
+Onze ticketverkoop is open van dinsdag tot vrijdag van 10u30 tot 13u00 en van 15u00 tot 18u00. 
+
+Met vriendelijke groeten, 
+
+De ploeg van Muziekpublique 
+
+Muziekpublique vzw
+Molière
+Naamsepoortgalerij
+Bolwerksquare 3
+02/217.26.00',
+'activite_reservation_message_confirmation_inscription_html' => '<p>Muziekpublique dankt u voor uw reservatie.</p>
+
+<p>Activiteit : @activite@</p>
+<p>Datum: @date@</p>
+<p>Gereserveerd op naam van: @nom@</p>
+
+<p>@inscriptions@</p>
+
+<p><b>Ticketverkoop</b></p>
+
+
+<p>Onze ticketverkoop is open van dinsdag tot vrijdag van 10u30-13u00 en van 15u00-18u00. 
+Muziekpublique | Molière | 02/217.26.00</p>',
+'adresse' => 'Adres',
+'affichage' => 'Weergave per pagina',
+'afficher_champ' => 'Veld weergeven',
+'affiner_recherche' => 'Verfijn uw opzoeking',
+'agenda' => 'Agenda',
+'agenda_complete' => 'Volledige agenda',
+'agenda_nl' => 'Rubriek:',
+'ajouter' => 'Reageren',
+'all' => 'Alles',
+'archive' => 'Archief',
+'article_27' => 'Wij aanvaarden Article 27',
+'article_27_legende' => 'Wij aanvaarden <a href=\'http://www.vgc.be/Cultuur/AlgemeenCultuurbeleidKunstenEnErfgoed/Cultuurwaardebon/PaspoortHome.htm\'>cultuurwaardebons </a> en Article 27',
+'article_presse' => 'Persartikel',
+
+
+// B
+'bien_a_vous' => 'Met vriendelijke groeten,',
+'billeterie' => 'Onze ticketing is van dinsdag tot vrijdag open tussen 10.30 en 13.00 en van 15.00 tot 18.00 in Theater Molière. ',
+
+
+// C
+'carte_de_membre' => 'Lidkaart',
+'categorie' => 'Categorie',
+'choisir' => 'Criteria van de selectie',
+'choisir_album' => 'Selecteer het album dat u wilt kopen',
+'choisir_genre' => 'Kies een genre',
+'choisir_groupe' => 'Kies een groep',
+'choisir_organisateur' => 'Kies een organisator',
+'choisir_pays' => 'Kies een land',
+'choisir_style' => 'kies een stijl',
+'choix_format_etiquettes' => 'Kies een formaat van een opgeslagen etiket',
+'code_postal' => 'Post',
+'code_postale' => 'Postcode',
+'commentaire' => 'Commentaren',
+'commentaires' => 'Commentaren',
+'comparer' => 'Vergelijken',
+'concert_futur' => 'Volgende',
+'concert_precedent' => 'Vorige ',
+'concerts_futurs' => 'Volgende concerten',
+'confirmation_reservation_titre' => 'Bevestiging van uw reservatie',
+'connectez_vous' => 'Als u al abonnee bent, kunt u zich aanmelden met het formulier hieronder',
+'consulter_liste' => 'De lijst raadplegen',
+'cours' => 'Naam van de cursus',
+'creation' => 'Datum creatie fiche',
+
+
+// D
+'date_debut' => 'Datum',
+'date_paiement' => 'Datum betaling',
+'de' => 'van',
+'delete' => 'Verwijderen',
+'derniers_inscrits' => 'De laatste inschrijvingen',
+'detailed_view' => 'Detailbeeld',
+'details_format_etiquette' => 'Details en wijzigingen van het formaat',
+'dossier_presse' => 'Persdossier',
+
+
+// E
+'edit' => 'Bewerk dit evenement',
+'effacer' => 'Annuleren',
+'email' => 'Email',
+'en_ligne' => 'Dernière apparence en ligne',
+'envoi_cd_possible' => 'Cd\'s beschikbaar op aanvraag:',
+'equipe_adresse_html' => '<p>De ploeg van Living Room Music Festival<br/>
+
+Muziekpublique, Molière Theater</p>
+<p>Naamsepoortgalerij</p>
+<p>Bolwerksquare 3<br/>
+1050 Elsene</p>
+<p>02/217.26.00.</p>',
+'erreur_horaires_doublons' => 'Gelieve 3 verschillende concerten om 3 verschillende uuren kiezen aub',
+'etiquettes' => 'Etiketten',
+'explication_reservation_concert' => '-<b>Gewoon tarief</b> : 
+ <br/>  <br/>
+Als je het onderstaande formulier invult, krijg je een bevestiging voor reservatie aan kassa-tarief. Uw reservatie blijft geldig tot 30 minuten voor het voorziene beginuur van het concert. Daarna kunnen de tickets terug te koop worden aangeboden.   
+ <br/>  <br/>
+-<b>Voorverkoop</b> :
+ <br/>  <br/>
+Om van de voorverkoopprijs te kunnen genieten, dien je het formulier hieronder in te vullen en
+ <br/>  <br/>
+1) ofwel de tickets in voorverkoop kopen in de kantoren van Muziekpublique, ten laatste de dag voor het concert, met vermelding van je naam, 
+ <br/>  <br/>
+2) ofwel betalen via overschrijving op rekening IBAN: BE22 7350 0490 4447, BIC: KREDBEBB (met vermelding van de datum van het concert) ten laatste 4 werkdagen voor het concert. 
+ De tickets zullen klaarliggen aan de kassa de avond van het concert.  <br/> <br/> 
+
+- <b>Ticketverkoop</b> :<br/> <br/> 
+
+Onze ticketverkoop is open van dinsdag tot vrijdag van 10u30-13u00 en van 15u00-18u00. <br/> 
+Muziekpublique | Molière | 02/217.26.00',
+
+
+// F
+'fax' => 'Fax',
+'fermer' => 'Sluiten',
+'fiche_technique' => 'Technishe fiche',
+'formulaire_inscription' => 'Inschrijvingsformulier',
+'from' => 'van',
+
+
+// G
+'gestion_paiements' => 'Gestion des paiements ouverts, total',
+'gratuit' => 'Gratis',
+
+
+// H
+'heures' => 'uur',
+'horaire' => 'Openingsuren',
+
+
+// I
+'id_asso' => 'Lidkaartnummer',
+'id_auteur' => 'id_auteur',
+'imprimer_plan' => 'Om de plattegrond van het festival te drukken, kan je klikken op de volgende link :',
+'indiquer_nom' => '<p style=\'color:red; text-align:center;\'>Afin de pouvoir enregistrer votre requette, veuillez indiquer un nom !</p>',
+'info_concerts' => 'Details Concert(en)',
+'info_cours' => 'Details Cursuss(en)',
+'infos' => 'Algemene informatie',
+'inscription' => 'Inschrijven',
+'inscription_provisoire_mail' => 'We vonden uw oorspronkelijke account',
+'inscriptions_partage' => 'Inschrijvingen met hetzelfde adres',
+'inscrire_benevole' => 'Een handje helpen ?',
+'interruption_cours' => 'Geen lessen op',
+'interruptions' => 'Geen lessen op',
+
+
+// J
+'jour' => 'Dag',
+'jusque_a' => 'tot',
+
+
+// L
+'label_prix_membres' => 'Ledenprijs',
+'label_prix_membres  ' => 'Ledenprijs',
+'label_prix_prevente_non_membres' => 'Voorverkoop niet-leden',
+'legende' => 'Legende',
+'lien_connecter' => 'Login',
+'liens' => 'Links',
+'lieu' => 'Plaats',
+'lire' => 'Lees',
+'lire_article' => 'Meer',
+'lire_tout' => 'Lees het hele artikel',
+
+
+// M
+'mail_confirmation' => 'U krijgt een bevestigingsmail met een overzicht van de door u geselecteerde concerten.',
+'maj' => 'Update',
+'membre' => 'Lid',
+'membre_echu_reabo' => 'Mijn abonnement is verlopen en ik wil graag mijn abonnement verlengen',
+'membre_souviens_rien' => 'Lid maar vergat email en login',
+'membres_gratuit' => 'Gratis voor leden',
+'meme' => 'Meer op',
+'meme_adresse' => 'We hebben al de nieuwsbrief ontvangen',
+'message_inscription_cours' => '<p>De inschrijvingen gebeuren per mail naar <a href=\'mailto:cursus@muziekpublique.be\'>cursus@muziekpublique.be</a> (naam, voornaam, gsm), of telefonisch op 02/217.26.00 en dient vervolgens betaald te worden op de rekening van Muziekpublique IBAN: BE22 7350 0490 4447, BIC: KREDBEBB.
+Opgelet, de betaling moet ons ten laatste twee weken voor het begin van de lessen bereiken.</p>
+<p>Het <a href=\'nieuws/concerten/Leden/\'>lidmaatschap</a> van Muziekpublique is verplicht om zich in te schrijven voor een cursus, maar is niet in de prijs inbegrepen. Tijdens het cursusjaar zal u over een lidkaart moeten beschikken</p>',
+'mobile' => 'GSM',
+'modifier' => 'Bewerken',
+'modify' => 'Wijzigen',
+'montant' => 'Bedrag',
+'montant_comptabilise' => 'Totaalbedrag ',
+'montant_difference' => 'Verschil',
+'monthly_view' => 'Overzicht per maand',
+'more' => 'Meer',
+
+
+// N
+'nom_famille' => 'Familienaam',
+'nom_numero' => 'Naam of nummer van de lidkaart van de persoon die ontvangt',
+'nom_site' => 'Naam van de site',
+'nombre_tickets' => 'Aantal tickets',
+'non_membre' => 'Geen lid',
+'nouveau_groupe' => 'Nieuwe groep',
+'nouveau_upload' => 'Nieuw upload',
+'numero' => 'Nummer',
+
+
+// O
+'opt_out' => '“Je persoonsgegevens worden door Muziekpublique verwerkt met het oog op de reservatie en om je op de hoogte te houden van haar acties en evenementen. Indien je dat laatste niet wenst, vink dan het vakje hiernaast aan.”',
+'origine' => 'Herkomst',
+
+
+// P
+'pages' => 'Pagina',
+'paiement' => 'Betaling',
+'partage' => 'Partage bulletin',
+'partagedonne' => 'Partage bulletin avec',
+'partager article' => 'Dit artikel delen',
+'partager_article' => 'Dit artikel delen',
+'pas_membre_inscrire' => 'Ik ben nooit lid geweest van Muziekpublique en ik zou graag een worden.',
+'pays' => 'Land',
+'periode_courante' => 'Lopende periode',
+'periode_cours' => 'Cursusperiode',
+'photo_presse' => 'Persfoto',
+'photos' => 'Foto\'s',
+'photos_visiteurs' => 'Foto\'s van onze bezoekers',
+'plan_festival' => 'Plattegrond van het festival',
+'plus' => 'Meer over',
+'possibilites_paiements' => 'Om de pass aan te kopen zijn er verscheidene mogelijkheden:',
+'pour_concerts_suivants' => 'Voor de volgende concerten :',
+'precedents' => 'Vorige',
+'prenom' => 'Voornaam',
+'prix' => 'Prijs',
+'prix_membres' => 'Toegang leden',
+'prix_non_membres' => 'Kassa niet-leden ',
+'prix_prevente_non_membres' => 'Voorverkoop niet-leden',
+'prochaine_evenement' => 'Volgende evenementen',
+'prochains' => 'Volgende',
+
+
+// R
+'recherche_manuel' => 'Doorzoek heel de lijst',
+'remercie_reservation_pour_le' => 'Muziekpublique dankt u voor uw reservatie voor het Living Room Music Festival van zondag 5 mei 2013.',
+'requete_defaut' => 'Etablir comme requette par défaut ?',
+'requete_existe_deja_changer_nom' => '<p style=\'color:red; text-align:center;\'>Une requete à ce nom existe déja, veuillez la garder sous un autre nom ou modifier l\'actuelle requette !</p>',
+'reservations' => 'Reserveren',
+'reserver' => 'Boek',
+'rue' => 'Straat',
+
+
+// S
+'save' => 'Opslaan',
+'save_as' => 'Opslaan als',
+'select_cours' => 'Een cursus kiezen',
+'select_date' => 'Kies een datum',
+'select_event' => 'Per evenement',
+'select_jour' => 'Een dag kiezen',
+'selection_donnees' => 'Choisir une requête enregistrée',
+'solde_imputer' => 'In te voeren bedrag',
+'sons' => 'Sounds',
+'statut' => 'Soort auteur',
+'statut_interne' => 'Statuut lid',
+'statut_provisoire' => 'Voorlopig statuut',
+'supprimer' => 'Verwijderen',
+
+
+// T
+'telecharger_photos' => 'foto\'s en audio downloaden',
+'telephone' => 'Telefoon',
+'texte_virement_cotisation' => '<p>Gelieve het bedrag van de lidkaart te storten: @montant@ euros  op onze rekening : @compte@, met vermelding lidkaart @annee@ : @id_asso@.</p>
+',
+'tickets_membre' => 'Tickets leden',
+'tickets_non_membre' => 'Tickets niet-leden ',
+'total' => 'Totaal',
+'tout' => 'Alles',
+'trie_par' => 'Afbeelden per',
+'trimestre' => 'cyclus (8 lessen)',
+
+
+// U
+'upload_images' => 'Upload foto\'s (login gevraagd)',
+'url_site' => 'Url van de Site',
+
+
+// V
+'valider' => 'Bevestigen',
+'validite' => 'Geldigheid lidkaart',
+'versement' => 'U kan ook een overschrijving doen op rekening IBAN: BE22 7350 0490 4447 (BIC: KREDBEBB ) ten laatste 4 werkdagen voor het festival met vermelding \'Living Room- aantal plaatsen’. De passen liggen de dag vanaf 13u00 klaar aan de naam van opdrachtgever aan het \'Check Point’ (Boekenwinkel NIJINSKI, Edelknaapstraat 15, 1050 Elsene).',
+'video' => 'Video(s)',
+'video_du' => 'Video van',
+'video_sing' => 'Videos',
+'ville' => 'Stad',
+'virement' => 'Overschrijving',
+'vous_venez_reserver' => 'U reserveerde:'
+
+
+);
+
+?>
